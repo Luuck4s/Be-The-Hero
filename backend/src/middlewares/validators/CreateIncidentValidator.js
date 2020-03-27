1ccb5f4d92ac98c47bd0232b98a5a8ae3230bc18
Lucas Andrade de Lima <lucass.limaa@outlook.com.br>
2020-03-27 09:27:16 -0300

    [backend] adição de validações das rotas

---
+++ backend/src/middlewares/validators/CreateIncidentValidator.js
@@ -0,0 +1,9 @@
+const { celebrate, Segments, Joi } = require("celebrate");
+
+module.exports = celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    value: Joi.number().required()
+  })
+});
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,22 +1,42 @@
 const express = require("express");
 
-const OngController = require('./controllers/OngController');
-const IncidentController = require('./controllers/IncidentController');
-const ProfileController = require('./controllers/ProfileController');
-const SessionController = require('./controllers/SessionController');
+const HeaderValidator = require("./middlewares/validators/HeaderValidator");
+
+const CreateOngValidator = require("./middlewares/validators/CreateOngValidator");
+
+const CreateIncidentValidator = require("./middlewares/validators/CreateIncidentValidator");
+const IncidentValidator = require("./middlewares/validators/IncidentValidator");
+const DeleteIncidentValidator = require("./middlewares/validators/DeleteIncidentValidator");
+
+const SessionValidator = require("./middlewares/validators/SessionValidator");
+
+const OngController = require("./controllers/OngController");
+const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
 
 const routes = express.Router();
 
-routes.post('/sessions', SessionController.create);
+routes.post("/sessions", SessionValidator, SessionController.create);
 
-routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
-routes.delete('/ongs', OngController.delete);
+routes.get("/ongs", OngController.index);
+routes.post("/ongs", CreateOngValidator, OngController.create);
+routes.delete("/ongs", HeaderValidator, OngController.delete);
 
-routes.get('/profile', ProfileController.index);
+routes.get("/profile", HeaderValidator, ProfileController.index);
 
-routes.get('/incidents', IncidentController.index)
-routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.get("/incidents", IncidentValidator, IncidentController.index);
+routes.post(
+  "/incidents",
+  HeaderValidator,
+  CreateIncidentValidator,
+  IncidentController.create
+);
+routes.delete(
+  "/incidents/:id",
+  HeaderValidator,
+  DeleteIncidentValidator,
+  IncidentController.delete
+);
 
 module.exports = routes;
